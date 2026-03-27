4ca593d1aa3125c8a012079ebaa85ced7397b777
Leon Liechti <37051290+Lon60@users.noreply.github.com>
2026-03-27 17:53:46 +0100

    Reorganize PATH additions and add alias for claude

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 
 fish_add_path $HOME/.opencode/bin
-export PATH="$HOME/.local/bin:$PATH"
-fish_add_path "$HOME/.bun/bin"
-fish_add_path "$HOME/go/bin"
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.bun/bin
+fish_add_path $HOME/go/bin
+fish_add_path $HOME/.lmstudio/bin
+alias claude="CLAUDE_CODE_EFFORT_LEVEL=max $HOME/.local/bin/claude --dangerously-skip-permissions"
